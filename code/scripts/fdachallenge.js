1c35231fc3d4d5c64f1bbc19c5693cb739a8c5fc
Doug Boude <dougboude@gmail.com>
2015-06-24 05:19:20 -0500

    added ability for grid to be exported to CSV; removed extraneous files; updated datatables library

--- code/scripts/fdachallenge.js
+++ code/scripts/fdachallenge.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
         } catch (e) {
         }
 
-        console.log(activeBars);
+        //console.log(activeBars);
 
         if (activeBars.length > 0) {
             // alert('congrats! You chose ' + activeBars[0].label + ' (' + activeBars[0].value + ' occurrences)\ncheck out what we logged in the console for the details on what is available during a chart click');
@@ -597,7 +597,7 @@ function createDTSDataSet(dset) {
         retDset.push(retRow);          
     });
 
-    console.log(retDset);
+    //console.log(retDset);
 
 
     //$("#tblcontainer").html('done. ');
@@ -638,12 +638,39 @@ function createDTSDataSet(dset) {
 
     $('#example').dataTable({
         "data": retDset,
-        "columns": keyWLDataColumns
+        "columns": keyWLDataColumns,
+        "oLanguage": {
+            "sSearch": "Type value to search on...&nbsp;&nbsp;",
+            "sZeroRecords": "There are no records that match your search criteria",
+            "sLengthMenu": '_MENU_'
+			},
+        "fnInitComplete": function () {
+            $('.dtlength label').prepend("Number of Records to Display:&nbsp;&nbsp;");
+			$('.btnExportGrid').append(
+				$('<button></button>')
+				.text('Export Table Data')
+				.on('click',function(){
+					JSONToCSVConvertor(prepTableDataForExport($('#example').dataTable().api().data(),keyWLDataColumns), $('#tier2modalhead').text(), true);
+				})
+			);
+        },	
+		"sDom": "<'btnExportGrid'><'clear'><'box-content'<'col-sm-2'f><'col-sm-10 text-right dtlength'l><'clearfix'>>rt<'box-content'<'col-sm-6'i><'col-sm-6 text-right'p><'clearfix'>>"
     });
-    
+}
 
-
-
+function prepTableDataForExport(dater,colnames){
+	var retval = [];
+	//for each item in the incoming dater, create array of objects, replacing the numeric key with the appropriate textual key
+	$(dater).each(function(index,childarray){
+		//loop over the child array
+		var thisobj = {};
+		$(childarray).each(function(i,val){
+			//setting the key by looking up the corresponding column name in the columns array
+			thisobj[colnames[i].sTitle] = val;
+		});
+		retval.push(thisobj);
+	});
+	return retval;
 }
 
 function listAllProperties(o) {
@@ -654,7 +681,78 @@ function listAllProperties(o) {
     for (objectToInspect = o; objectToInspect !== null; objectToInspect = Object.getPrototypeOf(objectToInspect)) {
         result = result.concat(Object.getOwnPropertyNames(objectToInspect));
     }
-
     return result;
+}
 
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    
+    var CSV = '';    
+    //Set Report title in first row or line
+    
+    CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+        
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+        
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+    
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+        
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {        
+        alert("Invalid data");
+        return;
+    }   
+    
+    //Generate a file name
+    var fileName = "MyReport_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");   
+    
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
